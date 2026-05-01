3e2acd392111673c4ef1a82f098058b3b31f0bd2
Ahmed Zahran <ahmed.zahran851@gmail.com>
2026-05-01 12:52:44 +0300

    Added cloudwatch alarms for lambda monitoring

---
+++ infra/monitoring.tf
@@ -0,0 +1,18 @@
+resource "aws_cloudwatch_metric_alarm" "lambda_error_alarm" {
+  alarm_name          = "visitor-counter-lambda-errors"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = 1
+  metric_name         = "Errors"
+  namespace           = "AWS/Lambda"
+  period              = 60 # Evaluates every 60 seconds
+  statistic           = "Sum"
+  threshold           = 1 # Triggers if there is 1 or more errors
+  alarm_description   = "This alarm monitors the Cloud Resume visitor counter Lambda for errors."
+  treat_missing_data  = "notBreaching"
+
+  dimensions = {
+    FunctionName = aws_lambda_function.visitor_counter_lambda.function_name
+  }
+
+  # alarm_actions = [aws_sns_topic.my_alerts.arn]
+}
